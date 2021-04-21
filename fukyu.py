4963a38c7c3dc90b2858de7da7191ce7dae66e67
Deya Berger <deyaberger@MacBook-Air-de-Deya.local>
2021-04-21 18:14:38 +0200

    avec graphik separe

--- fukyu.py
+++ fukyu.py
@@ -1,11 +1,6 @@
 import pandas as pd
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import numpy as np
 import time
-import random
-
+from graphik import Graph
 
 class Datapoint():
     def __init__(self, km, price, normalized_km):
@@ -71,58 +66,6 @@ class Dataset():
         self.index = 0
         raise StopIteration
     
-class Graph():
-    def __init__(self, irma, interactif=False):
-        self.interactif = interactif
-        self.linear_line = None
-        self.mse_curve = None
-        self.init_linear_graph(irma)
-        self.init_mse_graph()
-    
-    def init_linear_graph(self, irma):
-        fig = plt.figure(1)
-        plt.title("Linear Regression")
-        plt.xlabel("Mileage (km)")
-        plt.ylabel("Prices (USD)")
-        kms = list(irma.dataset.kms)
-        prices = list(irma.dataset.prices)
-        plt.scatter(kms, prices, color = "red", label="dataset values")
-        self.linear_x = np.linspace(0, irma.dataset.kms.max())
-        plt.legend()
-        plt.grid(True)
-        fig.canvas.manager.window.attributes("-topmost", 1)
-        if not self.interactif:
-            plt.pause(0.001)
-    
-    def init_mse_graph(self):
-        fig = plt.figure(2)
-        plt.title("MSE cost evolution")
-        plt.xlabel("Episode")
-        plt.ylabel("Cost")
-        self.mse_x, self.mse_y = [], []
-        fig.canvas.manager.window.attributes("-topmost", 0)
-        plt.pause(0.001)
-    
-    def remove_plot(self, a_plot):
-        if a_plot:
-            line = a_plot.pop(0)
-            line.remove()
-    
-    def update_linear_graph(self, theta0, theta1):
-        plt.figure(1)
-        self.remove_plot(self.linear_line)
-        self.linear_y = theta0 + (theta1 * self.linear_x)
-        self.linear_line = plt.plot(self.linear_x, self.linear_y, color="blue", label="predict function")
-        if not self.interactif:
-            plt.pause(0.001)
-    
-    def update_mse_graph(self, cost, episode):
-        plt.figure(2)
-        self.remove_plot(self.mse_curve)
-        self.mse_x.append(episode)
-        self.mse_y.append(cost)
-        self.mse_curve = plt.plot(self.mse_x, self.mse_y, color="orange", label="cost evolution")
-        plt.pause(0.001)
     
 class Irma():
     def __init__(self, dataset):
@@ -232,5 +175,4 @@ if __name__ == "__main__" :
     datasetto =  Dataset(path = "data.csv")
     irma = Irma(datasetto)
     irma.training_loop(plot = True)
-    # irma.test_for_graph()
     plt.show()
---
+++ graphik.py
@@ -0,0 +1,58 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+class Graph():
+    def __init__(self, irma, interactif=False):
+        self.interactif = interactif
+        self.linear_line = None
+        self.mse_curve = None
+        self.init_linear_graph(irma)
+        self.init_mse_graph()
+    
+    def init_linear_graph(self, irma):
+        fig = plt.figure(1)
+        plt.title("Linear Regression")
+        plt.xlabel("Mileage (km)")
+        plt.ylabel("Prices (USD)")
+        kms = list(irma.dataset.kms)
+        prices = list(irma.dataset.prices)
+        plt.scatter(kms, prices, color = "red", label="dataset values")
+        self.linear_x = np.linspace(0, irma.dataset.kms.max())
+        plt.legend()
+        plt.grid(True)
+        fig.canvas.manager.window.attributes("-topmost", 1)
+        if not self.interactif:
+            plt.pause(0.001)
+    
+    def init_mse_graph(self):
+        fig = plt.figure(2)
+        plt.title("MSE cost evolution")
+        plt.xlabel("Episode")
+        plt.ylabel("Cost")
+        self.mse_x, self.mse_y = [], []
+        fig.canvas.manager.window.attributes("-topmost", 0)
+        plt.pause(0.001)
+    
+    def remove_plot(self, a_plot):
+        if a_plot:
+            line = a_plot.pop(0)
+            line.remove()
+            
+    def update_linear_graph(self, theta0, theta1):
+        plt.figure(1)
+        self.remove_plot(self.linear_line)
+        self.linear_y = theta0 + (theta1 * self.linear_x)
+        self.linear_line = plt.plot(self.linear_x, self.linear_y, color="blue", label="predict function")
+        if not self.interactif:
+            plt.pause(0.001)
+    
+    def update_mse_graph(self, cost, episode):
+        plt.figure(2)
+        self.remove_plot(self.mse_curve)
+        self.mse_x.append(episode)
+        self.mse_y.append(cost)
+        self.mse_curve = plt.plot(self.mse_x, self.mse_y, color="orange", label="cost evolution")
+        plt.pause(0.001)
