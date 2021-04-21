6eb089d0881361103d924db740f7b17966d7b6fa
Deya Berger <deyaberger@MacBook-Air-de-Deya.local>
2021-04-21 18:00:47 +0200

    avec des graphs good

--- fukyu.py
+++ fukyu.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import random
+
 
 class Datapoint():
     def __init__(self, km, price, normalized_km):
@@ -67,6 +71,58 @@ class Dataset():
         self.index = 0
         raise StopIteration
     
+class Graph():
+    def __init__(self, irma, interactif=False):
+        self.interactif = interactif
+        self.linear_line = None
+        self.mse_curve = None
+        self.init_linear_graph(irma)
+        self.init_mse_graph()
+    
+    def init_linear_graph(self, irma):
+        fig = plt.figure(1)
+        plt.title("Linear Regression")
+        plt.xlabel("Mileage (km)")
+        plt.ylabel("Prices (USD)")
+        kms = list(irma.dataset.kms)
+        prices = list(irma.dataset.prices)
+        plt.scatter(kms, prices, color = "red", label="dataset values")
+        self.linear_x = np.linspace(0, irma.dataset.kms.max())
+        plt.legend()
+        plt.grid(True)
+        fig.canvas.manager.window.attributes("-topmost", 1)
+        if not self.interactif:
+            plt.pause(0.001)
+    
+    def init_mse_graph(self):
+        fig = plt.figure(2)
+        plt.title("MSE cost evolution")
+        plt.xlabel("Episode")
+        plt.ylabel("Cost")
+        self.mse_x, self.mse_y = [], []
+        fig.canvas.manager.window.attributes("-topmost", 0)
+        plt.pause(0.001)
+    
+    def remove_plot(self, a_plot):
+        if a_plot:
+            line = a_plot.pop(0)
+            line.remove()
+    
+    def update_linear_graph(self, theta0, theta1):
+        plt.figure(1)
+        self.remove_plot(self.linear_line)
+        self.linear_y = theta0 + (theta1 * self.linear_x)
+        self.linear_line = plt.plot(self.linear_x, self.linear_y, color="blue", label="predict function")
+        if not self.interactif:
+            plt.pause(0.001)
+    
+    def update_mse_graph(self, cost, episode):
+        plt.figure(2)
+        self.remove_plot(self.mse_curve)
+        self.mse_x.append(episode)
+        self.mse_y.append(cost)
+        self.mse_curve = plt.plot(self.mse_x, self.mse_y, color="orange", label="cost evolution")
+        plt.pause(0.001)
     
 class Irma():
     def __init__(self, dataset):
@@ -140,70 +196,21 @@ class Irma():
         self.learning_rate = self.learning_rate * self.learning_rate_decay
         self.theta0, self.theta1 = theta0, theta1
         
-    def init_graph(self):
-        plt.figure(1)
-        plt.title("Linear Regression")
-        plt.xlabel("Mileage (km)")
-        plt.ylabel("Prices (USD)")
-        kms = list(self.dataset.kms)
-        prices = list(self.dataset.prices)
-        x = np.linspace(0, self.dataset.kms.max())
-        y = self.theta0 + (self.theta1 * x)
-        plt.scatter(kms, prices, color = "red", label="prices by km")
-        current_plot = plt.plot(x, y, color="blue", label="predict function")
-        plt.legend()
-        plt.grid(True)
-        plt.pause(0.001)
-        plt.figure(2)
-        plt.title("MSE cost evolution")
-        plt.xlabel("Episode")
-        plt.ylabel("Cost")
-        c = [3]
-        e = [0]
-        current_cost = plt.plot(e, c, color="red")
-        plt.pause(0.5)
-        return (x, current_plot, c, e, current_cost)
-    
-    def update_graph(self, x, current_plot, c, e, current_cost):
-        plt.figure(1)
-        y = self.theta0 + (self.theta1 * x)
-        line = current_plot.pop(0)
-        line.remove()
-        current_plot = plt.plot(x, y, color="blue", label="predict function")
-        plt.pause(0.001)
-        plt.figure(2)
-        line = current_cost.pop(0)
-        line.remove()
-        current_cost = plt.plot(e, c, color="red")
-        plt.pause(0.5)
-        return (x, current_plot, c, e, current_cost)
-    
-    def test_for_graph(self):
-        x, current_plot, c, e, current_cost = self.init_graph()
-        time.sleep(4)
-        for i in range(15):
-            print(f"i = {i}")
-            self.theta0 += 100
-            self.theta1 -= 0.01
-            c.append(c[i] - 0.3)
-            e.append(i)
-            x, current_plot, c, e, current_cost = self.update_graph(x, current_plot, c, e, current_cost)
-        # fig, ax = plt.subplots()
-        # x = np.linspace(0, self.dataset.kms.max())
-        # y = self.theta0 + (self.theta1 * x)
-        # kms = list(self.dataset.kms)
-        # prices = list(self.dataset.prices)
-        # ax.plot(x, y)
-        # ax.scatter(kms, prices, color = "red", label="prices by km")
-        # ax.set_title('Linear Regression')
-            
 
     def training_loop(self, plot=False):
         go_on = True
         if plot == True:
-            x, current_plot = self.init_graph()
+            graphismus = Graph(irma)
+            time.sleep(2)
         self.newcost = self.mean_squared_error_dataset(self.theta0, self.theta1)
+        i = 0
         while (go_on):
+            if plot == True:
+                graphismus.update_linear_graph(self.theta0, self.theta1)
+                graphismus.update_mse_graph(self.newcost, i)
+                if graphismus.interactif == True:
+                    plt.figure(1)
+                    plt.waitforbuttonpress()
             self.oldcost = self.newcost
             self.update_thetas()
             self.newcost = self.mean_squared_error_dataset(self.theta0, self.theta1)
@@ -212,9 +219,8 @@ class Irma():
                 self.decrease_and_assign(self.middle_theta0, self.middle_theta1)
             if (self.newcost > self.oldcost):
                 self.decrease_and_assign(self.oldtheta0, self.oldtheta1)
-            if plot == True:
-                x, current_plot = self.update_graph(x, current_plot)
             print(self)
+            i += 1
             
     def __str__(self):
         return (f"error: {self.newcost:.2E}, \tthetas: [{self.theta0}, {self.theta1}], lr: {self.learning_rate}")
@@ -225,6 +231,6 @@ class Irma():
 if __name__ == "__main__" :
     datasetto =  Dataset(path = "data.csv")
     irma = Irma(datasetto)
-    # irma.training_loop(plot = True)
-    irma.test_for_graph()
+    irma.training_loop(plot = True)
+    # irma.test_for_graph()
     plt.show()
